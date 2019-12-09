ae0e7af1508789f9f861a7cab92355877697d4fc
songlairui <songlairui@126.com>
2019-12-09 16:47:29 +0800

    chore(dock): dock

---
+++ .vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    "flowchart",
+    [
+      "vuepress-plugin-comment",
+      {
+        choosen: "gitalk",
+        options: {
+          clientID: "771bbb3960244fec0d14",
+          clientSecret: "0296371f3e4027f55b1de4bda159bcb353e6007a",
+          repo: "GitHub reposyzl/calli-doc",
+          owner: "songlairui",
+          admin: [
+            "songlairui"
+          ],
+          distractionFreeMode: false
+        }
+      }
+    ]
+  ]
+};
